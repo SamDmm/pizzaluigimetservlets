897f07640839a16532337a94d9103469dee2ca76
Sam <Sam@192.168.0.163>
2019-02-28 11:14:42 +0100

    Servlet en jsp combineren

--- src/be/vdab/servlets/IndexServlet.java
+++ src/be/vdab/servlets/IndexServlet.java
@@ -5,23 +5,20 @@ import java.io.PrintWriter;
 import java.time.LocalDateTime;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//@WebServlet("/index.htm")
+@WebServlet("/index.htm")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+	private static final String VIEW = "/WEB-INF/JSP/index.jsp";
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		out.println("<!doctype html>");
-		out.println("<html lang='nl'><head>");
-		out.println("<title>Pizza Luigi</title></head>");
-		out.println("<body><h1>");
 		int uur = LocalDateTime.now().getHour();
-		out.print(uur >= 6 && uur < 12 ? "Goede morgen" : uur >= 12 && uur < 18 ? "Goede middag" : "Goede avond");
-		out.println("</h1></body></html>");
+		request.setAttribute("begroeting", uur >= 6 && uur < 12 ? "Goede morgen" : uur >= 12  && uur < 18 ? "Goede middag" : "Goede avond");
+		request.getRequestDispatcher(VIEW).forward(request, response);
 	}
 
 }

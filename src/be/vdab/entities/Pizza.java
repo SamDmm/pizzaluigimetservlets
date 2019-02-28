f1d457798b56359f3f946962a2ff0cc68edbe90a
Sam <Sam@192.168.0.163>
2019-02-28 15:33:03 +0100

    JSTL

---
+++ src/be/vdab/entities/Pizza.java
@@ -0,0 +1,61 @@
+package be.vdab.entities;
+
+import java.math.BigDecimal;
+
+public class Pizza {
+	long id;
+	private String naam;
+	private BigDecimal prijs;
+	private boolean pikant;
+	
+	public Pizza(String naam, BigDecimal prijs, boolean pikant) {
+		setNaam(naam);
+		setPrijs(prijs);
+		setPikant(pikant);
+	}
+	public Pizza(long id, String naam, BigDecimal prijs, boolean pikant) {
+		this(naam, prijs, pikant);
+		setId(id);
+	}
+
+	public long getId() {
+		return id;
+	}
+	public String getNaam() {
+		return naam;
+	}
+	public BigDecimal getPrijs() {
+		return prijs;
+	}
+	public boolean isPikant() {
+		return pikant;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	public static boolean isNaamValid(String naam) {
+		return naam != null && !naam.trim().isEmpty();
+	}
+	public void setNaam(String naam) {
+		if (! isNaamValid(naam)) {
+			throw new IllegalArgumentException();
+		}
+		this.naam = naam;
+	}
+	public static boolean isPrijsValid(BigDecimal prijs) {
+		return prijs != null && prijs.compareTo(BigDecimal.ZERO) >= 0;
+	}
+	public void setPrijs(BigDecimal prijs) {
+		if (! isPrijsValid(prijs)) {
+			throw new IllegalArgumentException();
+		}
+		this.prijs = prijs;
+	}
+	public void setPikant(boolean pikant) {
+		this.pikant = pikant;
+	}
+	
+	
+	
+}

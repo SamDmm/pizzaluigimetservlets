0adeb1cec0e03d0c6f251c91d058a8561bfea2e1
Sam <Sam@192.168.0.163>
2019-03-02 22:49:02 +0100

    Bestand uploaden

--- WebContent/WEB-INF/JSP/pizzas.jsp
+++ WebContent/WEB-INF/JSP/pizzas.jsp
@@ -19,6 +19,10 @@
           <c:param name="id" value='${pizza.id}'/>
         </c:url>
         <a href='${detailURL}'>Detail</a>
+        <c:if test='${pizzaIdsMetFoto.contains(pizza.id)}'>
+          <c:url value='/pizzafotos/${pizza.id}.jpg' var='fotoURL'/>
+          <a href='${fotoURL}'>Foto</a>
+        </c:if>
       </li>
     </c:forEach>
   </ul>
--- src/be/vdab/servlets/PizzaServlet.java
+++ src/be/vdab/servlets/PizzaServlet.java
@@ -1,7 +1,11 @@
 package be.vdab.servlets;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import be.vdab.entities.Pizza;
 import be.vdab.repositories.PizzaRepository;
 
 @WebServlet("/pizzas.htm")
@@ -17,15 +22,23 @@ public class PizzaServlet extends HttpServlet {
 	private static final String VIEW = "/WEB-INF/JSP/pizzas.jsp";
 	private static final String PIZZA_REQUESTS = "pizzasRequests";
 	private final PizzaRepository pizzaRepository = new PizzaRepository();
+	private String pizzaFotosPad;
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		((AtomicInteger) this.getServletContext().getAttribute(PIZZA_REQUESTS)).incrementAndGet();
-		request.setAttribute("pizzas", pizzaRepository.findAll());
+		List<Pizza> pizzas = pizzaRepository.findAll();
+		request.setAttribute("pizzas", pizzas);
+		request.setAttribute("pizzaIdsMetFoto", 
+				pizzas.stream()
+				.filter(pizza -> Files.exists(Paths.get(pizzaFotosPad, pizza.getId() + ".jpg")))
+				.map(pizza -> pizza.getId())
+				.collect(Collectors.toList()));
 		request.getRequestDispatcher(VIEW).forward(request, response);
 	}
 	@Override
 	public void init() throws ServletException {
 		this.getServletContext().setAttribute(PIZZA_REQUESTS, new AtomicInteger());
+		pizzaFotosPad = this.getServletContext().getRealPath("/pizzafotos");
 	}
 
 }
